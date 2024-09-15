cd142c3a86bf976312558e50c141ef802cb74b97
nguyenanhcd <anh487303@gmail.com>
2024-09-15 17:41:14 +0700

    UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Box sx={{ margin: '6px', width: '200px' }}>
-        <CustomTextField id='outlined-multiline-flexible' label='Multiline' />
-      </Box>
+      <Box sx={{ margin: '6px', width: '200px' }}></Box>
     </>
   )
 }
---
+++ src/views/layout/HorizontalLayout.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import { NextPage } from 'next'
+import { styled } from '@mui/material/styles'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Badge from '@mui/material/Badge'
+
+const drawerWidth: number = 240
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean
+}
+
+type TProps = {
+  open?: boolean
+  toggleDrawer: () => void
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open'
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
+
+const HorizontalLayout: NextPage<TProps> = ({ open, toggleDrawer }) => {
+  return (
+    <AppBar position='absolute' open={open}>
+      <Toolbar
+        sx={{
+          pr: '24px'
+        }}
+      >
+        <IconButton
+          edge='start'
+          color='inherit'
+          aria-label='open drawer'
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: '36px',
+            ...(open && { display: 'none' })
+          }}
+        ></IconButton>
+        <Typography component='h1' variant='h6' color='inherit' noWrap sx={{ flexGrow: 1 }}>
+          Dashboard
+        </Typography>
+        <IconButton color='inherit'>
+          <Badge badgeContent={4} color='secondary'>
+            {/* <NotificationsIcon /> */}
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default HorizontalLayout
