99ec389f4370bb23f64a30ac0d4f4a4d5efea229
nguyenanhcd <anh487303@gmail.com>
2024-10-01 14:38:42 +0700

    done remember feature

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/router'
 import { ReactNode, ReactElement, useEffect } from 'react'
 import { ACCESS_TOKEN, USER_DATA } from 'src/configs/auth'
-import { clearLocalUserData } from 'src/helpers/storage'
+import { clearLocalUserData, clearTemporaryToken, getTemporaryToken } from 'src/helpers/storage'
 import { useAuth } from 'src/hooks/useAuth'
 
 interface AuthGuardProps {
@@ -16,12 +16,15 @@ const AuthGuard = (props: AuthGuardProps) => {
   const router = useRouter()
 
   useEffect(() => {
+    const { temporaryToken } = getTemporaryToken()
+
     if (!router.isReady) return
 
     if (
       authContext.user === null &&
       !window.localStorage.getItem(ACCESS_TOKEN) &&
-      !window.localStorage.getItem(USER_DATA)
+      !window.localStorage.getItem(USER_DATA) &&
+      !temporaryToken
     ) {
       if (router.asPath !== '/' && router.asPath !== '/login') {
         router.replace({
@@ -35,6 +38,19 @@ const AuthGuard = (props: AuthGuardProps) => {
       clearLocalUserData()
     }
   }, [router.route])
+
+  useEffect(() => {
+    const handleUnload = () => {
+      clearTemporaryToken()
+    }
+
+    // this event will be dispatched after the closing or reloading of the current page
+    window.addEventListener('beforeunload', handleUnload)
+
+    return () => {
+      window.addEventListener('beforeunload', handleUnload)
+    }
+  })
 
   if (authContext.loading || authContext.user === null) {
     return fallback
