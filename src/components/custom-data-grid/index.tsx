cebaace1231dac54ca35919eb53ea035633e58c7
nguyenanhcd <anh487303@gmail.com>
2024-10-04 07:58:52 +0700

    custom datagrid

---
+++ src/components/custom-data-grid/index.tsx
@@ -0,0 +1,20 @@
+import { DataGrid, DataGridProps } from '@mui/x-data-grid'
+import { Box, styled } from '@mui/material'
+import { forwardRef, Ref } from 'react'
+
+const StyleCustomGrid = styled(DataGrid)<DataGridProps>(({ theme }) => ({
+  '& .MuiDataGrid-main': {
+    border: `1px solid ${theme.palette.customColors.borderColor}`,
+    borderRadius: '8px'
+  }
+}))
+
+const CustomDataGrid = forwardRef((props: DataGridProps, ref: Ref<any>) => {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <StyleCustomGrid {...props} />
+    </Box>
+  )
+})
+
+export default CustomDataGrid
